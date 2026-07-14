84a49275b6afc9dfe7fe88301de4d2bf02197a99
abdul Rafay <abdulrafay700000@gmail.com>
2026-07-14 23:47:14 +0500

    f: Collection Fetch & Ui

--- Project/src/pages/CollectionPage.jsx
+++ Project/src/pages/CollectionPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import ResultCard from '../components/ResultCard'
 
 const CollectionPage = () => {
 
     const collection = JSON.parse(localStorage.getItem('collection'))
 
   return (
-    <div style={{color:'white'}}>
+    <div style={{color:'white'}} className='main'>
           {collection.map((item,idx) => (
-               <div key={idx}>
-                     
+               <div key={idx} className='card'>
+                   <ResultCard key={idx} item={item}/>
                </div>
           ))}
     </div>
